f416a1454981dd0470d2052e3713606b2f72c37b
danny <danny@thaicapital.com>
2021-01-31 03:55:14 -0500

    update 01

---
+++ django_tutorial_project/blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='blog-home'), # views.home tells it to look at views.py for function home; use blog-home instead of home to simplify future needs for lookup, always smart to be clear and specific
+    path('about/', views.about, name='blog-about'),
+]
