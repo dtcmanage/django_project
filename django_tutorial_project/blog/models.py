2e4f21b20de5156ff6d78ebc804fbd6ba435add6
danny <danny@thaicapital.com>
2021-02-06 23:57:06 -0500

    Update 3

--- django_tutorial_project/blog/models.py
+++ django_tutorial_project/blog/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db import models 
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now) #a few options: auto_now is better to use for when post are edited or updated; auto_now_add is too static, cannot change later
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+def __str__(self):
+    return self.title
--- django_tutorial_project/blog/views.py
+++ django_tutorial_project/blog/views.py
@@ -1,27 +1,12 @@
 from django import http
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'DannyTCM',
-        'title': 'Blog Post 1',
-        'content': 'First Post content',
-        'date_posted': 'Feb 6, 2021',
-    },
-    {
-        'author': 'Jan Doe',
-        'title': 'Blog Post 2',
-        'content': 'second Post content',
-        'date_posted': 'Feb 6, 2021',
-    }
-    
-
-]
 
 def home(request):
     context={
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
